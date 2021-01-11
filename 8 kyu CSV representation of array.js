411fa65eb274b136b0638967e342389cf4413706
saxenification <saxenborn.andreas@hotmail.com>
2021-01-11 05:06:41 +0100

    Update 8 kyu CSV representation of array.js

--- 8 kyu CSV representation of array.js
+++ 8 kyu CSV representation of array.js
@@ -1,5 +1,6 @@
 /**Create a function that returns the CSV representation of a two-dimensional numeric array.
-Example:
+ */
+/*Example:
 input:
    [[ 0, 1, 2, 3, 4 ],
     [ 10,11,12,13,14 ],
@@ -9,10 +10,21 @@ output:
      '0,1,2,3,4\n'
     +'10,11,12,13,14\n'
     +'20,21,22,23,24\n'
-    +'30,31,32,33,34'
++'30,31,32,33,34'
 Array's length > 2.
 */
 
 function toCsvText(array) {
-
- }
+    let CSV = []
+for(pick of array){
+    for(number of pick){
+        CSV.push(number.toString())
+    }
+    CSV.push("/n")
+}
+return CSV.join()
+}
+console.log(toCsvText([[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]]))
